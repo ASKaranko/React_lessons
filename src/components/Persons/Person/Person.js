0af9a370e935e0d1a5b68484ff72068030d1fd9c
Karanko Andrei <andr.karanko@gmail.com>
2021-03-03 15:42:21 +0200

    Implemented splitting an App into components - Module 6

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -23,10 +23,6 @@ const person = (props) => {
 	// 		width: '450px',
 	// 	}
 	// };
-	const rnd = Math.random();
-	if (rnd > 0.7) {
-		throw new Error('Something went wrong');
-	}
 	return (
 			// <div className="Person" style={style}>
 			// 	<StyledDiv>
--- src/containers/App.js
+++ src/containers/App.js
@@ -17,8 +17,8 @@ Now you can use className="Post"
 anywhere in your app and receive that styling.
  */
 import classes from './App.css';
-import Person from "./Person/Person";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 // const StyledButton = styled.button`
 //   background-color: ${props => props.alt ? 'red' : 'green'};
@@ -96,24 +96,14 @@ class App extends Component {
     // };
 
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
 
-      persons = (
-          <div>
-            {this.state.persons.map((person, index) => {
-              return <ErrorBoundary key={person.id}>
-                <Person
-                      click={() => this.deletePersonHandler(index)}
-                      name={person.name}
-                      age={person.age}
-                      changed={(event) => this.nameChangedHandler(event, person.id)}
-                    />
-              </ErrorBoundary>
-            })}
-          </div>
-      );
+      persons = <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+            />;
 			// style.backgroundColor = 'red';
 			// style[':hover'] = {
       //   backgroundColor: 'tomato',
@@ -121,30 +111,19 @@ class App extends Component {
       // }
 
       //Синтаксис с css-модулями
-      btnClass = classes.Red;
+      // btnClass = classes.Red;
     }
 
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-		}
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
-		}
-
     return (
-
           <div className={classes.App}>
-            <h1>Hi, I'm a React App</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button
-                className={btnClass}
-                onClick={this.togglePersonsHandler}>Toggle Persons
-            </button>
+            <Cockpit
+              title={this.props.appTitle}
+              showPersons={this.state.showPersons}
+              persons={this.state.persons}
+              clicked={this.togglePersonsHandler}
+            />
             {persons}
           </div>
-
-
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Hi, I\'m React App'));
   }
